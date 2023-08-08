59aaccb1a3c2669ff63374c662ca58b352b42c8b
Styledev <dewey.bushaw@parapxl.com>
2023-08-08 08:44:04 -0600

    Updating maxRounds form 5,000 to 100,000 to better avoid rematches.

--- matchupsGenerator.js
+++ matchupsGenerator.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const seedrandom = require("./seedrandom");
 
-const maxRounds = 5000;
+const maxRounds = 100000;
 
 if (!process.argv[2]) {
   console.error("Missing cycle argument.");
